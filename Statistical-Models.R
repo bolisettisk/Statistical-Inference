02dab94bc6f766a8a04bcb82baa234cbed7eed3f
bolisettisk <siva.karteek@gmail.com>
2021-10-16 22:31:42 +0100

    .

---
+++ Statistical-Models.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(dslabs)
+d <- 0.039
+Ns <- c(1298, 533, 1342, 897, 774, 254, 812, 324, 1291, 1056, 2172, 516)
+p <- (d + 1) / 2
+
+polls <- map_df(Ns, function(N) {
+  x <- sample(c(0,1), size=N, replace=TRUE, prob=c(1-p, p))
+  x_hat <- mean(x)
+  se_hat <- sqrt(x_hat * (1 - x_hat) / N)
+  list(estimate = 2 * x_hat - 1, 
+       low = 2*(x_hat - 1.96*se_hat) - 1, 
+       high = 2*(x_hat + 1.96*se_hat) - 1,
+       sample_size = N)
+}) %>% mutate(poll = seq_along(Ns))
+
+#--------------------------------------------------------------
+
+cat("\014")
+
+data(polls_us_election_2016)
+
+polls <- polls_us_election_2016 %>% 
+  filter(state == "U.S." & enddate >= "2016-10-31" &
+           (grade %in% c("A+","A","A-","B+") | is.na(grade)))
+
+polls <- polls %>% 
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100)
